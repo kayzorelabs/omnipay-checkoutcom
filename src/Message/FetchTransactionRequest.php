3f90c53fe3370083d3ea0fddd6324a1f359383f9
Dany <dmbalanda@iraiser.eu>
2019-03-11 14:39:53 +0100

    ADD Capture completePruchaseResponse message

---
+++ src/Message/FetchTransactionRequest.php
@@ -0,0 +1,50 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: dany
+ * Date: 11/03/19
+ * Time: 09:49
+ */
+
+namespace Omnipay\CheckoutCom\Message;
+
+
+class FetchTransactionRequest extends  AbstractRequest
+{
+
+    public function getData()
+    {
+
+        $data = array();
+
+        $data['trackId'] = $this->getTrackId();
+        $data['description'] = $this->getDescription();
+
+        if ($udf = $this->getUdfValues()) {
+            $data['udf1'] = $udf[0];
+            $data['udf2'] = isset($udf[1]) ? $udf[1] : null;
+            $data['udf3'] = isset($udf[2]) ? $udf[2] : null;
+            $data['udf4'] = isset($udf[3]) ? $udf[3] : null;
+            $data['udf5'] = isset($udf[4]) ? $udf[4] : null;
+        }
+
+
+        return $data;
+    }
+
+    public function sendData($data)
+    {
+        $httpResponse = $this->sendRequest($data);
+
+        return $this->response = new FetchTransactionResponse($this, $httpResponse);
+    }
+    public function getEndpoint()
+    {
+        if($this->getTestMode()) {
+            return parent::getEndpoint() . '/charges/charge_test_' . $this->getTransactionReference() ;
+        }else{
+        return parent::getEndpoint() . '/charges/charge_' . $this->getTransactionReference();
+    }
+    }
+
+}
